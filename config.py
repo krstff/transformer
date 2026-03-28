ed00f83ffe71a36b4d15bf65f23c9ee2a7a67019
krstff <krystof.matustik@seznam.cz>
2026-03-28 21:38:34 +0100

    feat: Attention mechanism

---
+++ config.py
@@ -0,0 +1,9 @@
+# Standard GPT hyperparameters
+BATCH_SIZE = 32
+BLOCK_SIZE = 128      # Maximum context length (sequence length)
+EMBED_SIZE = 256      # Dimensionality of the embeddings
+NUM_HEADS = 8         # Number of attention heads
+NUM_BLOCKS = 6        # Number of transformer blocks
+ATTENTION_DROP = 0.1
+RESID_DROP = 0.1
+LEARNING_RATE = 3e-4
--- model.py
+++ model.py
@@ -17,3 +17,61 @@ class FeedForward(nn.Module):
         y = self.activaiton(y)
 
         return y
+
+class MSMAttention(nn.Module):
+    def __init__(self, input_dim, num_embed, num_heads):
+        super().__init__()
+        self.num_heads = num_heads
+        self.head_dim = num_embed // num_heads
+
+        # Create mask for masked attention, registered as buffer as this is not a learnable parameter
+        self.register_buffer("mask", torch.tril(torch.ones(config.BLOCK_SIZE, config.BLOCK_SIZE))
+                                     .view(1, 1, config.BLOCK_SIZE, config.BLOCK_SIZE))
+
+        # Projection for calculating Q K V
+        # could also be three different projections (=> num_embed * 3)
+        self.qkv = nn.Linear(input_dim, num_embed * 3)
+        self.linear = nn.Linear(input_dim, num_embed)
+
+        # Dropout for the attention layer (is disabled during model.Eval())
+        self.a_dropout = nn.Dropout(config.ATTENTION_DROP)
+        self.resid_dropout = nn.Dropout(config.RESID_DROP)
+    
+    def forward(self, x):
+        # batch size, sequence length, embedding size (equal to input_dim)
+        B, T, C = x.size()
+
+        qkv = self.qkv(x)
+        # split into heads
+        qkv = rearrange(qkv, 'b t (three h d) -> b t h (three d)', three=3, h=self.num_heads)
+        # transpose for dimension matching
+        qkv = rearrange(qkv, 'b t h (three d) -> b h t (three d)', three=3)
+
+        # split into q k v
+        # each of dimension (B, num_heads, T, head_dim)
+        q, k, v = qkv.chunk(3, dim=-1)
+
+        e = q @ k.transpose(-1, -2)
+        e = e / math.sqrt(k.size(-1))
+
+        # Mask the lower triangle and compute softmax
+        a = a.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        a = e.softmax(a, dim=-1)
+        a = self.a_dropout(a)
+
+        y = a @ v
+        # merge heads together
+        y = y.rearrange(y, 'b h t d -> b t (h d)')
+
+        y = self.linear(y)
+        y = self.resid_dropout(y)
+
+        return y
+
+
+
+
+
+
+
+    
