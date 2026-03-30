9bd852b2f8c48670f991f25f6e640c85ca205f00
krstff <krystof.matustik@seznam.cz>
2026-03-30 17:57:18 +0200

    chore: vibecoded data loader

--- dataset.py
+++ dataset.py
@@ -1,15 +1,39 @@
 import torch
 import config
 import tiktoken
+import os
+import glob
+import lance
 
 
 class DataHandler():
-    def __init__(self, filename, tokenizer):
-        with open(filename, 'r', encoding='utf-8') as f:
-            text = f.read()
-
+    def __init__(self, path, tokenizer, is_lance=False):
         self.enc = tokenizer
+        text = ""
+        
+        if is_lance:
+            ds = lance.dataset(path)
+            table = ds.to_table(columns=['text'])[:10000] 
+            
+            text_list = table['text'].to_pylist()
+            text = " \n\n ".join(text_list)
+
+        elif os.path.isdir(path):
+            files = glob.glob(os.path.join(path, '*.txt'))
+            print(f"Found {len(files)} files in directory. Loading into memory...")
+            
+            # !! Loads the entire dataset into RAM
+            for f_name in files:
+                with open(f_name, 'r', encoding='utf-8') as f:
+                    text += f.read() + " \n\n " 
+                    
+        elif os.path.isfile(path):
+            with open(path, 'r', encoding='utf-8') as f:
+                text = f.read()
+
+        print("Tokenizing data...")
         self.data = torch.tensor(self.enc.encode(text), dtype=torch.long)
+        print(f"Dataset loaded successfully! Total tokens: {len(self.data):,}")
 
     def get_batch(self):
         ix = torch.randint(len(self.data) - config.BLOCK_SIZE, (config.BATCH_SIZE,))
